9b3788643cd6b5047646d130635a6b73f698f0ee
Kacper Lukasz Nowicki <nokacper24@gmail.com>
2023-10-20 21:02:51 +0200

    remove all files

--- lib/providers/meals_provider.dart
+++
@@ -1,8 +0,0 @@
-import "package:flutter_riverpod/flutter_riverpod.dart";
-import "package:meals_app/data/dummy_data.dart";
-
-/// The meals provider. Returns [dummyMeals] as the value.
-final mealsProvider = Provider((ref) {
-  return dummyMeals;
-});
-
